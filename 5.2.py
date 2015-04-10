7ccd2c1adbf1eb38ef2f04fa7e552b47c74b195c
Polina Leonova <polina.ostapenko.88@gmail.com>
2015-04-10 11:16:57 +0300

    5.2with proposed version

--- 5.2.py
+++ 5.2.py
@@ -1,15 +1,31 @@
 __author__ = 'polina'
 
+
 def counter(a, b):
-    list1 = str(b)
-    l = set(list1)
-    for i in l:
-        res=0
-        if i.find('5') == -1:
-            res+=1
-            print 'klkl'
-    print res
+    str_a = str(a)
+    list_b = str(list(set(str(b))))
+    list_res = []
+    for i in str_a :
+        if list_b.find(i) != -1:
+            list_res.append(i)
+    return len(set(list_res))
 
 
-counter(12345, 56766)
+#
+# counter(98123560, 79266)
 
+
+#
+# proposed version
+
+
+# def counter(a, b):
+#     num = 0
+#     a_str = str(a)
+#     b_str = str(b)
+#     found = ''
+#     for char_b in b_str:
+#         if a_str.find(char_b) != -1 and found.find(char_b) == -1:
+#             num = num + 1
+#             found = found + char_b
+#     return num
