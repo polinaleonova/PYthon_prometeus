264c89686aaebd592ac0b607f4f7d19d2fd6e118
Polina Leonova <polina.ostapenko.88@gmail.com>
2015-04-15 13:01:05 +0300

    5.4-modif,6.1,6.2,6.3,6.4-added

---
+++ 6.4.py
@@ -0,0 +1,13 @@
+__author__ = 'polina'
+
+def saddle_point(matrix_list):
+    g = ()
+    r = (i for i in matrix_list[1])
+        # g = matrix_list[i][j]
+    print r
+
+
+
+
+
+saddle_point([[1,2,3],[3,2,1]])
