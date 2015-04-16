63eb3d58e04e56b5e94170ed09fd67553425f33a
Polina Leonova <polina.ostapenko.88@gmail.com>
2015-04-16 13:58:33 +0300

    5.4with proposed version

--- 6.3.py
+++ 6.3.py
@@ -1,13 +1,25 @@
 __author__ = 'polina'
 
 def saddle_point(matrix_list):
-    g = ()
-    r = (i for i in matrix_list[1])
-        # g = matrix_list[i][j]
-    print r
+    d = {}
+    a = []
+    print a
+    b = []
+    for i in range(len(matrix_list)):
+        for j in range(len(matrix_list[0])):
+            d[matrix_list[i][j]] = (i, j)
+            a.append(j)
+
+    b = d.items()
+    # for n in d.values():
+    #     if
+
+
+    print a
 
 
 
 
 
-saddle_point([[1,2,3],[3,2,1]])
+
+saddle_point([[1,2,3],[0,4,6]])
---
+++ 6.5!.py
@@ -0,0 +1,14 @@
+__author__ = 'polina'
+
+def convert_n_to_m(x, n, m):
+    if 1<=n and m<=36 and type(x) == int or type(x) ==str and x%n ==0:
+
+    else:
+        return False
+
+
+
+convert_n_to_m(x, n, m)
+
+
+
